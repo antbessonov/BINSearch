be697e5dfeda6561f7e2b321d483f64388c93fef
Anton Bessonov <anton@bessonov.org>
2023-02-14 20:26:52 +0700

    Добавление анотации AndroidEntryPoint в MainActivity.kt

--- app/src/main/java/com/example/binsearch/MainActivity.kt
+++ app/src/main/java/com/example/binsearch/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.binsearch.ui.theme.BINSearchTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
