4c9481b16b4f045a7940e968b27ca5522cce124e
Anton Bessonov <anton@bessonov.org>
2023-02-14 14:51:05 +0700

    Создание ProgressBarLoading.kt

---
+++ app/src/main/java/com/example/binsearch/ui/component/ProgressBarLoading.kt
@@ -0,0 +1,38 @@
+package com.example.binsearch.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProgressBarLoading(
+    modifier: Modifier,
+    isLoading: Boolean
+) {
+    if (isLoading) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                color = Color.Blue,
+                strokeWidth = 5.dp,
+                modifier = modifier.size(60.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ProgressBarLoadingPreview() {
+    ProgressBarLoading(isLoading = true, modifier = Modifier)
+}
