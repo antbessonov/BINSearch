d97d6ab1d65f8c10bcdefdb8b59774377f6ba998
Anton Bessonov <anton@bessonov.org>
2023-02-14 13:42:56 +0700

    Создание CardInfoDto.kt network model

---
+++ data/src/main/java/com/example/binsearch/data/network/model/CardInfoDto.kt
@@ -0,0 +1,15 @@
+package com.example.binsearch.data.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CardInfoDto(
+    @SerializedName("number")
+    val numberCard: NumberCardDto,
+    val scheme: String,
+    val type: String,
+    val brand: String,
+    val prepaid: Boolean,
+    val country: CountryDto,
+    val bank: BankDto
+)
+
