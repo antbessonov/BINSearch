2d75a1b8f9295795ffa680049bf95b60fcdd3b62
Anton Bessonov <anton@bessonov.org>
2023-02-14 20:18:46 +0700

    Добавление ErrorMessageContent() и SearchCardInfoScreen() в SearchCardInfoScreen.kt

--- app/src/main/java/com/example/binsearch/ui/screen/SearchCardInfoScreen.kt
+++ app/src/main/java/com/example/binsearch/ui/screen/SearchCardInfoScreen.kt
@@ -1,14 +1,19 @@
 package com.example.binsearch.ui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
@@ -18,11 +23,49 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.binsearch.R
+import com.example.binsearch.domain.util.ErrorMessage
+import com.example.binsearch.ui.component.DialogError
+import com.example.binsearch.ui.component.ProgressBarLoading
+import com.example.binsearch.viewmodel.SearchCardInfoViewModel
 
 @Composable
-fun SearchCardInfoScreen() {
+fun SearchCardInfoScreen(
+    modifier: Modifier,
+    searchCardInfoViewModel: SearchCardInfoViewModel = hiltViewModel()
+) {
 
+    val searchCardInfoState by searchCardInfoViewModel.searchCardInfoState.collectAsState()
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        SearchField(
+            modifier = modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.8f)
+                .padding(top = 16.dp),
+            value = searchCardInfoState.binCard,
+            onValueChange = searchCardInfoViewModel::updateBINCard,
+            onClickIconButton = searchCardInfoViewModel::getCardInfo,
+            onFocusChange = searchCardInfoViewModel::hideCardInfo,
+            isBINValid = searchCardInfoState.isBINValid
+        )
+    }
+    ProgressBarLoading(modifier = modifier, isLoading = searchCardInfoState.isLoadingProgressBar)
+    searchCardInfoState.errorMessage?.let {
+        ErrorMessageContent(
+            modifier = modifier,
+            onDialogDismiss = searchCardInfoViewModel::hideErrorMessage,
+            onButtonClick = searchCardInfoViewModel::hideErrorMessage,
+            errorMessage = it
+        )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -90,6 +133,38 @@ private fun SearchField(
         }),
         singleLine = true
     )
+}
+
+@Composable
+private fun ErrorMessageContent(
+    modifier: Modifier,
+    onDialogDismiss: () -> Unit,
+    onButtonClick: () -> Unit,
+    errorMessage: ErrorMessage,
+) {
+    when (errorMessage) {
+        ErrorMessage.BINNotFound -> DialogError(
+            modifier = modifier,
+            onDialogDismiss = onDialogDismiss,
+            onButtonClick = onButtonClick,
+            errorTitle = stringResource(R.string.BIN_not_found),
+            errorDescription = stringResource(R.string.error_BIN_not_found_description),
+        )
+        ErrorMessage.NetworkProblem -> DialogError(
+            modifier = modifier,
+            onDialogDismiss = onDialogDismiss,
+            onButtonClick = onButtonClick,
+            errorTitle = stringResource(R.string.network_problem),
+            errorDescription = stringResource(R.string.error_network_problem_description),
+        )
+        ErrorMessage.SomethingWentWrong -> DialogError(
+            modifier = modifier,
+            onDialogDismiss = onDialogDismiss,
+            onButtonClick = onButtonClick,
+            errorTitle = stringResource(R.string.something_went_wrong),
+            errorDescription = stringResource(R.string.error_something_went_wrong_description),
+        )
+    }
 }
 
 @Preview
