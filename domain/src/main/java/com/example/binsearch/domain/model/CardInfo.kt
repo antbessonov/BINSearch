d4ae20531b656fc3d6ccf60a707186fceca8d30b
Anton Bessonov <anton@bessonov.org>
2023-02-14 12:48:33 +0700

    Создание CardInfo.kt domain model

---
+++ domain/src/main/java/com/example/binsearch/domain/model/CardInfo.kt
@@ -0,0 +1,11 @@
+package com.example.binsearch.domain.model
+
+data class CardInfo(
+    val numberCard: NumberCard,
+    val scheme: String?,
+    val type: String?,
+    val brand: String?,
+    val prepaid: Boolean?,
+    val country: Country,
+    val bank: Bank
+)
