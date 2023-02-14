585ce83540bcbd501ea064652a3b30b788cd7fdd
Anton Bessonov <anton@bessonov.org>
2023-02-14 19:57:20 +0700

    Создание CardInfoUI.kt

---
+++ app/src/main/java/com/example/binsearch/ui/model/CardInfoUI.kt
@@ -0,0 +1,12 @@
+package com.example.binsearch.ui.model
+
+data class CardInfoUI(
+    val length: String,
+    val luhn: String,
+    val country: String,
+    val scheme: String,
+    val type: String,
+    val brand: String,
+    val prepaid: String,
+    val bank: String
+)
