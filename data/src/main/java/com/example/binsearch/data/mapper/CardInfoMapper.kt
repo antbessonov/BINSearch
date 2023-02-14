10ed8c4d50ceef1dabe9d8bc7c734b8e41c1de71
Anton Bessonov <anton@bessonov.org>
2023-02-14 14:01:22 +0700

    Создание CardInfoMapper и mapResponseToState()

---
+++ data/src/main/java/com/example/binsearch/data/mapper/CardInfoMapper.kt
@@ -0,0 +1,63 @@
+package com.example.binsearch.data.mapper
+
+import com.example.binsearch.data.network.model.BankDto
+import com.example.binsearch.data.network.model.CardInfoDto
+import com.example.binsearch.data.network.model.CountryDto
+import com.example.binsearch.data.network.model.NumberCardDto
+import com.example.binsearch.domain.model.Bank
+import com.example.binsearch.domain.model.CardInfo
+import com.example.binsearch.domain.model.Country
+import com.example.binsearch.domain.model.NumberCard
+import com.example.binsearch.domain.util.ErrorMessage
+import com.example.binsearch.domain.util.LoadingState
+import retrofit2.Response
+
+class CardInfoMapper {
+
+    fun mapResponseToState(response: Response<CardInfoDto>): LoadingState<CardInfo> {
+        return when (response.code()) {
+            in 200..299 -> {
+                val responseBody = response.body() ?: throw Exception("Response body is null.")
+                val cardInfo = mapDtoToEntity(responseBody)
+                LoadingState.Success(cardInfo)
+            }
+            404 -> LoadingState.Error(message = ErrorMessage.BINNotFound)
+            else -> LoadingState.Error(message = ErrorMessage.SomethingWentWrong)
+        }
+    }
+
+
+    private fun mapDtoToEntity(dto: CardInfoDto): CardInfo {
+        return CardInfo(
+            numberCard = mapNumberCardDtoToEntity(numberCardDto = dto.numberCard),
+            scheme = dto.scheme,
+            type = dto.type,
+            brand = dto.brand,
+            prepaid = dto.prepaid,
+            country = mapCountryDtoToEntity(countryDto = dto.country),
+            bank = mapBankDtoToEntity(bankDto = dto.bank)
+        )
+    }
+
+    private fun mapNumberCardDtoToEntity(numberCardDto: NumberCardDto) = NumberCard(
+        length = numberCardDto.length,
+        luhn = numberCardDto.luhn
+    )
+
+    private fun mapCountryDtoToEntity(countryDto: CountryDto) = Country(
+        numeric = countryDto.numeric,
+        alpha2 = countryDto.alpha2,
+        name = countryDto.name,
+        emoji = countryDto.emoji,
+        currency = countryDto.currency,
+        latitude = countryDto.latitude,
+        longitude = countryDto.longitude
+    )
+
+    private fun mapBankDtoToEntity(bankDto: BankDto) = Bank(
+        name = bankDto.name,
+        url = bankDto.url,
+        phone = bankDto.phone,
+        city = bankDto.city
+    )
+}
