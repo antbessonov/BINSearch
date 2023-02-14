db9e7c787b35aa718470eedf252ef29cca753168
Anton Bessonov <anton@bessonov.org>
2023-02-14 20:27:43 +0700

    Создание DomainModule.kt

---
+++ app/src/main/java/com/example/binsearch/di/DomainModule.kt
@@ -0,0 +1,18 @@
+package com.example.binsearch.di
+
+import com.example.binsearch.domain.repository.AppRepository
+import com.example.binsearch.domain.usecase.GetCardInfoUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object DomainModule {
+
+    @Provides
+    fun provideGetCardInfoUseCase(repository: AppRepository): GetCardInfoUseCase {
+        return GetCardInfoUseCase(repository = repository)
+    }
+}
