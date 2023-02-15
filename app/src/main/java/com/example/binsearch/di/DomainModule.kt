b7ddcaafeceb92793b591964c192d5f6744f5986
Anton Bessonov <anton@bessonov.org>
2023-02-15 17:07:41 +0700

    Добавление provideGetBINRequestListUseCase() в DomainModule.kt

--- app/src/main/java/com/example/binsearch/di/DomainModule.kt
+++ app/src/main/java/com/example/binsearch/di/DomainModule.kt
@@ -1,6 +1,7 @@
 package com.example.binsearch.di
 
 import com.example.binsearch.domain.repository.AppRepository
+import com.example.binsearch.domain.usecase.GetBINRequestListUseCase
 import com.example.binsearch.domain.usecase.GetCardInfoUseCase
 import dagger.Module
 import dagger.Provides
@@ -14,5 +15,10 @@ object DomainModule {
     @Provides
     fun provideGetCardInfoUseCase(repository: AppRepository): GetCardInfoUseCase {
         return GetCardInfoUseCase(repository = repository)
+    }
+
+    @Provides
+    fun provideGetBINRequestListUseCase(repository: AppRepository): GetBINRequestListUseCase {
+        return GetBINRequestListUseCase(repository = repository)
     }
 }
