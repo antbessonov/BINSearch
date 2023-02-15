359df09a2cccc7be8ce84022bbc3117d03d0cf94
Anton Bessonov <anton@bessonov.org>
2023-02-15 12:53:52 +0700

    Добавление getBINRequestList() в AppRepository.kt

--- domain/src/main/java/com/example/binsearch/domain/repository/AppRepository.kt
+++ domain/src/main/java/com/example/binsearch/domain/repository/AppRepository.kt
@@ -1,9 +1,13 @@
 package com.example.binsearch.domain.repository
 
+import com.example.binsearch.domain.model.BINRequest
 import com.example.binsearch.domain.model.CardInfo
 import com.example.binsearch.domain.util.LoadingState
+import kotlinx.coroutines.flow.Flow
 
 interface AppRepository {
 
     suspend fun getCardInfo(binCard: Int): LoadingState<CardInfo>
+
+    fun getBINRequestList(): Flow<List<BINRequest>>
 }
