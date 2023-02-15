5b0e57ba5cbbeea470ec8932f7e001ff616c66a6
Anton Bessonov <anton@bessonov.org>
2023-02-15 17:42:08 +0700

    Добавление suspend к getBINRequestList() и изменение возвращаемого типа

--- domain/src/main/java/com/example/binsearch/domain/repository/AppRepository.kt
+++ domain/src/main/java/com/example/binsearch/domain/repository/AppRepository.kt
@@ -3,11 +3,10 @@ package com.example.binsearch.domain.repository
 import com.example.binsearch.domain.model.BINRequest
 import com.example.binsearch.domain.model.CardInfo
 import com.example.binsearch.domain.util.LoadingState
-import kotlinx.coroutines.flow.Flow
 
 interface AppRepository {
 
     suspend fun getCardInfo(binCard: Int): LoadingState<CardInfo>
 
-    fun getBINRequestList(): Flow<List<BINRequest>>
+    suspend fun getBINRequestList(): List<BINRequest>
 }
