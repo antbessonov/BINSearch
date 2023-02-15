4406ecaab6f877071d3a9973e5e75c04deeeeed1
Anton Bessonov <anton@bessonov.org>
2023-02-15 17:44:01 +0700

    Добавление suspend в getBINRequestList() и измменение возвращаемого типа в BINRequestDao.kt

--- data/src/main/java/com/example/binsearch/data/database/BINRequestDao.kt
+++ data/src/main/java/com/example/binsearch/data/database/BINRequestDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.binsearch.data.database.model.BINRequestDbModel
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BINRequestDao {
@@ -14,5 +13,5 @@ interface BINRequestDao {
     suspend fun insertBINRequest(binRequest: BINRequestDbModel)
 
     @Query("SELECT * FROM bin_requests ORDER BY time DESC")
-    fun getBINRequestList(): Flow<List<BINRequestDbModel>>
+    suspend fun getBINRequestList(): List<BINRequestDbModel>
 }
