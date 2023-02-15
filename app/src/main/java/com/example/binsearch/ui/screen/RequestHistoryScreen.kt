4d91365985f9469688f2932c8b63258023b7f9f2
Anton Bessonov <anton@bessonov.org>
2023-02-15 17:20:10 +0700

    Добавление реализации RequestHistoryScreen.kt

--- app/src/main/java/com/example/binsearch/ui/screen/RequestHistoryScreen.kt
+++ app/src/main/java/com/example/binsearch/ui/screen/RequestHistoryScreen.kt
@@ -1,8 +1,63 @@
 package com.example.binsearch.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.binsearch.domain.model.BINRequest
+import com.example.binsearch.viewmodel.RequestHistoryViewModel
 
 @Composable
-fun RequestHistoryScreen() {
+fun RequestHistoryScreen(
+    modifier: Modifier,
+    requestHistoryViewModel: RequestHistoryViewModel = hiltViewModel()
+) {
+    val requestHistoryState by requestHistoryViewModel.requestHistoryState.collectAsStateWithLifecycle()
+    BinRequestList(modifier = modifier, binRequestList = requestHistoryState.binRequestList)
+}
 
+@Composable
+private fun BinRequestList(
+    modifier: Modifier,
+    binRequestList: List<BINRequest>
+) {
+    LazyColumn {
+        items(binRequestList) { binRequest ->
+            BinRequestItem(modifier = modifier, binRequest = binRequest)
+        }
+    }
+}
+
+@Composable
+private fun BinRequestItem(
+    modifier: Modifier,
+    binRequest: BINRequest
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 2.dp, bottom = 2.dp)
+    ) {
+        Column(modifier = modifier) {
+            Text(text = binRequest.time)
+            Text(text = binRequest.binCard.toString())
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BinRequestItemPreview() {
+    val binRequest = BINRequest(time = "14:35 11.12.21", binCard = 45689900)
+    BinRequestItem(modifier = Modifier, binRequest = binRequest)
 }
