81a21f64c948ef464ee385b7efa0146e1322de9a
Anton Bessonov <anton@bessonov.org>
2023-02-15 20:46:45 +0700

    Добавление convertLongToTime() в BINRequestMapper.kt

--- data/src/main/java/com/example/binsearch/data/mapper/BINRequestMapper.kt
+++ data/src/main/java/com/example/binsearch/data/mapper/BINRequestMapper.kt
@@ -2,10 +2,19 @@ package com.example.binsearch.data.mapper
 
 import com.example.binsearch.data.database.model.BINRequestDbModel
 import com.example.binsearch.domain.model.BINRequest
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BINRequestMapper {
 
     fun mapDbModelToEntity(dbModel: BINRequestDbModel): BINRequest {
-        return BINRequest(time = dbModel.time.toString(), binCard = dbModel.binCard)
+        return BINRequest(time = convertLongToTime(time = dbModel.time), binCard = dbModel.binCard)
+    }
+
+    private fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val pattern = "dd.MM.yyyy HH:mm"
+        val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+        return sdf.format(date)
     }
 }
