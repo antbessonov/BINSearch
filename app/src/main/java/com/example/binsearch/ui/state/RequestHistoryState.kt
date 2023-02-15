537d59d68e59ab2398379208d96c4d345af029f3
Anton Bessonov <anton@bessonov.org>
2023-02-15 20:48:53 +0700

    Добавление isErrorMessage в RequestHistoryState.kt

--- app/src/main/java/com/example/binsearch/ui/state/RequestHistoryState.kt
+++ app/src/main/java/com/example/binsearch/ui/state/RequestHistoryState.kt
@@ -3,5 +3,6 @@ package com.example.binsearch.ui.state
 import com.example.binsearch.domain.model.BINRequest
 
 data class RequestHistoryState(
-    val binRequestList: List<BINRequest> = emptyList()
+    val binRequestList: List<BINRequest> = emptyList(),
+    val isErrorMessage: Boolean = false
 )
