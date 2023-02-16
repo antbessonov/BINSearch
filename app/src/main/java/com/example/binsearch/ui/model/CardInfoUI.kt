415078f2fd3b11252f69f009b75ab39c3f06b6f1
Anton Bessonov <anton@bessonov.org>
2023-02-16 22:14:59 +0700

    Изменение типа country и bank в CardInfoUI.kt

--- app/src/main/java/com/example/binsearch/ui/model/CardInfoUI.kt
+++ app/src/main/java/com/example/binsearch/ui/model/CardInfoUI.kt
@@ -3,10 +3,10 @@ package com.example.binsearch.ui.model
 data class CardInfoUI(
     val length: String,
     val luhn: String,
-    val country: String,
+    val country: CountryUi,
     val scheme: String,
     val type: String,
     val brand: String,
     val prepaid: String,
-    val bank: String
+    val bank: BankUi
 )
