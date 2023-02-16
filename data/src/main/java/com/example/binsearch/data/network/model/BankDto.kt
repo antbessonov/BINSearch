3cb56aab225df97ef87c0eb72726ddc13c32f044
Anton Bessonov <anton@bessonov.org>
2023-02-16 22:10:01 +0700

    Добавление нулабельности к полям в BankDto.kt

--- data/src/main/java/com/example/binsearch/data/network/model/BankDto.kt
+++ data/src/main/java/com/example/binsearch/data/network/model/BankDto.kt
@@ -1,8 +1,8 @@
 package com.example.binsearch.data.network.model
 
 data class BankDto(
-    val name: String,
-    val url: String,
-    val phone: String,
-    val city: String,
+    val name: String?,
+    val url: String?,
+    val phone: String?,
+    val city: String?,
 )
