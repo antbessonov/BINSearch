72aaeaa5aea2f8f431867310c2a72c592011bc99
Anton Bessonov <anton@bessonov.org>
2023-02-16 22:02:20 +0700

    Изменение типа message в LoadingState.kt

--- domain/src/main/java/com/example/binsearch/domain/util/LoadingState.kt
+++ domain/src/main/java/com/example/binsearch/domain/util/LoadingState.kt
@@ -2,5 +2,5 @@ package com.example.binsearch.domain.util
 
 sealed class LoadingState<out T> {
     data class Success<out T>(val value: T) : LoadingState<T>()
-    data class Error(val message: ErrorMessage) : LoadingState<Nothing>()
+    data class Error(val message: LoadingError) : LoadingState<Nothing>()
 }
