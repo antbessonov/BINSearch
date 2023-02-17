9b5c05b296980cf192f4650d40f68c44461cde70
Anton Bessonov <anton@bessonov.org>
2023-02-17 19:22:13 +0700

    Добавление style для Text и создание NoHistory в RequestHistoryScreen.kt

--- app/src/main/java/com/example/binsearch/ui/screen/RequestHistoryScreen.kt
+++ app/src/main/java/com/example/binsearch/ui/screen/RequestHistoryScreen.kt
@@ -1,8 +1,6 @@
 package com.example.binsearch.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -12,15 +10,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.binsearch.R
 import com.example.binsearch.domain.model.BINRequest
 import com.example.binsearch.ui.component.DialogError
+import com.example.binsearch.ui.theme.replyTypography
 import com.example.binsearch.viewmodel.RequestHistoryViewModel
 
 @Composable
@@ -30,7 +27,11 @@ fun RequestHistoryScreen(
 ) {
     val requestHistoryState by requestHistoryViewModel.requestHistoryState.collectAsStateWithLifecycle()
 
-    BinRequestList(modifier = modifier, binRequestList = requestHistoryState.binRequestList)
+    if (requestHistoryState.binRequestList.isEmpty()) {
+        NoHistory(modifier = modifier)
+    } else {
+        BinRequestList(modifier = modifier, binRequestList = requestHistoryState.binRequestList)
+    }
     if (requestHistoryState.isErrorMessage) {
         DialogError(
             modifier = modifier,
@@ -38,6 +39,17 @@ fun RequestHistoryScreen(
             onButtonClick = requestHistoryViewModel::hideErrorMessage,
             errorTitle = stringResource(R.string.something_went_wrong),
             errorDescription = stringResource(R.string.error_something_went_wrong_description),
+        )
+    }
+}
+
+@Composable
+private fun NoHistory(modifier: Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(
+            modifier = modifier,
+            text = stringResource(R.string.no_search_history),
+            style = replyTypography.titleMedium,
         )
     }
 }
@@ -64,14 +76,21 @@ private fun BinRequestItem(
             .fillMaxWidth()
             .padding(2.dp)
     ) {
-        Column(modifier = modifier.padding(8.dp).fillMaxWidth()) {
+        Column(
+            modifier = modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
             Text(
                 modifier = modifier.padding(start = 8.dp),
                 text = binRequest.binCard,
-                fontWeight = FontWeight.ExtraBold,
-                fontSize = 24.sp
+                style = replyTypography.headlineSmall
             )
-            Text(modifier = modifier.align(alignment = Alignment.End), text = binRequest.time)
+            Text(
+                modifier = modifier.align(alignment = Alignment.End),
+                text = binRequest.time,
+                style = replyTypography.bodySmall
+            )
         }
     }
 }
@@ -81,4 +100,10 @@ private fun BinRequestItem(
 private fun BinRequestItemPreview() {
     val binRequest = BINRequest(time = "14:35 11.12.21", binCard = "99778")
     BinRequestItem(modifier = Modifier, binRequest = binRequest)
+}
+
+@Preview
+@Composable
+private fun NoHistoryPreview(){
+    NoHistory(modifier = Modifier)
 }
