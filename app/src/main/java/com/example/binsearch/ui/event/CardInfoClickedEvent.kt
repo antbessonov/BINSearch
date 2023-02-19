f9186fe13bfdc311a238267f1ee998c3d00f0882
Anton Bessonov <anton@bessonov.org>
2023-02-19 21:35:54 +0700

    Удаление context из CardInfoClickedEvent

--- app/src/main/java/com/example/binsearch/ui/event/CardInfoClickedEvent.kt
+++ app/src/main/java/com/example/binsearch/ui/event/CardInfoClickedEvent.kt
@@ -1,20 +1,9 @@
 package com.example.binsearch.ui.event
 
-import android.content.Context
-
 sealed interface CardInfoClickedEvent
 
-data class BankUrlClicked(
-    val context: Context,
-    val bankUrl: String
-) : CardInfoClickedEvent
+data class BankUrlClicked(val bankUrl: String) : CardInfoClickedEvent
 
-data class BankPhoneClicked(
-    val context: Context,
-    val bankPhone: String
-) : CardInfoClickedEvent
+data class BankPhoneClicked(val bankPhone: String) : CardInfoClickedEvent
 
-data class CountryCoordinatesClicked(
-    val context: Context,
-    val countryCoordinates: String
-) : CardInfoClickedEvent
+data class CountryCoordinatesClicked(val countryCoordinates: String) : CardInfoClickedEvent
