6a6ed45442870d80472d36fa0a011306759626d5
Anton Bessonov <anton@bessonov.org>
2023-02-20 12:46:02 +0700

    Изменение goToPhoneApp(), goToBrowser(), goToMapApp() в SystemNavigator.kt

--- app/src/main/java/com/example/binsearch/ui/navigation/SystemNavigator.kt
+++ app/src/main/java/com/example/binsearch/ui/navigation/SystemNavigator.kt
@@ -1,27 +1,33 @@
 package com.example.binsearch.ui.navigation
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
 
-class SystemNavigator @Inject constructor(
-    @ApplicationContext private val context: Context
-) {
-
-    fun goToPhoneApp(phone: String) {
+fun goToPhoneApp(context: Context, phone: String, handleNavigationError: () -> Unit) {
+    try {
         val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone"))
         context.startActivity(callIntent)
+    } catch (e: ActivityNotFoundException) {
+        handleNavigationError()
     }
+}
 
-    fun goToBrowser(url: String) {
+fun goToBrowser(context: Context, url: String, handleNavigationError: () -> Unit) {
+    try {
         val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://$url"))
         context.startActivity(webIntent)
+    } catch (e: ActivityNotFoundException) {
+        handleNavigationError()
     }
+}
 
-    fun goToMapApp(coordinates: String) {
+fun goToMapApp(context: Context, coordinates: String, handleNavigationError: () -> Unit) {
+    try {
         val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:$coordinates"))
         context.startActivity(mapIntent)
+    } catch (e: ActivityNotFoundException) {
+        handleNavigationError()
     }
 }
