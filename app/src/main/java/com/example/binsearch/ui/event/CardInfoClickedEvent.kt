6dadc6e26490606e3d341fb8f371ef69414c1e7e
Anton Bessonov <anton@bessonov.org>
2023-02-20 12:41:22 +0700

    Удаление CardInfoClickedEvent.kt

--- app/src/main/java/com/example/binsearch/ui/event/CardInfoClickedEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.example.binsearch.ui.event
-
-sealed interface CardInfoClickedEvent
-
-data class BankUrlClicked(val bankUrl: String) : CardInfoClickedEvent
-
-data class BankPhoneClicked(val bankPhone: String) : CardInfoClickedEvent
-
-data class CountryCoordinatesClicked(val countryCoordinates: String) : CardInfoClickedEvent
